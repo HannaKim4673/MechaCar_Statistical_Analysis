43f0877eee2cf5a22b98735693115ff1d501f725
unknown <hkim4673@gmail.com>
2021-06-26 19:31:07 -0500

    Updated code for suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,8 @@ mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table) #Generates multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table)) #Generates summary statistics
+
+suspensioncoilPSI <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #Imports and reads in csv file with suspension coil data as a dataframe
+
+total_summary <- suspensioncoilPSI %>% summarize(PSI_mean=mean(PSI),PSI_Median=median(PSI),PSI_Variance=(sd(PSI) * sd(PSI)),PSI_Stdev=sd(PSI)) #Generates summary statistics table for suspension coil data
+lot_summary <- suspensioncoilPSI %>% group_by(Manufacturing_Lot) %>% summarize(PSI_mean=mean(PSI),PSI_Median=median(PSI),PSI_Variance=(sd(PSI) * sd(PSI)),PSI_Stdev=sd(PSI)) #Generates summary statistics for each manufacturing lot  
